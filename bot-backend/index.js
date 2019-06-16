2a5cd7144b83a84b24ebbc076c8944973030cee5
urbangeek <gupta.amlan@gmail.com>
2019-06-16 02:57:39 -0400

    cleaning

--- bot-backend/index.js
+++ bot-backend/index.js
@@ -75,77 +75,12 @@ bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
 
 					}
 
-
 				  });
 		    });
 
 		  });
 
-		
-
-
-
 	});
-
-
 });
 
-// bot.hear(['food', 'hungry'], (payload, chat) => {
-// 	// Send a text message with quick replies
-// 	chat.say({
-// 		text: 'What do you want to eat today?',
-// 		quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
-// 	});
-// });
-
-// bot.hear(['help'], (payload, chat) => {
-// 	// Send a text message with buttons
-// 	chat.say({
-// 		text: 'What do you need help with?',
-// 		buttons: [
-// 			{ type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
-// 			{ type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
-// 			{ type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
-// 		]
-// 	});
-// });
-
-// bot.hear('image', (payload, chat) => {
-// 	// Send an attachment
-// 	chat.say({
-// 		attachment: 'image',
-// 		url: 'http://example.com/image.png'
-// 	});
-// });
-
-// bot.hear('ask me something', (payload, chat) => {
-
-// 	const askName = (convo) => {
-// 		convo.ask(`What's your name?`, (payload, convo) => {
-// 			const text = payload.message.text;
-// 			convo.set('name', text);
-// 			convo.say(`Oh, your name is ${text}`).then(() => askFavoriteFood(convo));
-// 		});
-// 	};
-
-// 	const askFavoriteFood = (convo) => {
-// 		convo.ask(`What's your favorite food?`, (payload, convo) => {
-// 			const text = payload.message.text;
-// 			convo.set('food', text);
-// 			convo.say(`Got it, your favorite food is ${text}`).then(() => sendSummary(convo));
-// 		});
-// 	};
-
-// 	const sendSummary = (convo) => {
-// 		convo.say(`Ok, here's what you told me about you:
-// 	      - Name: ${convo.get('name')}
-// 	      - Favorite Food: ${convo.get('food')}`);
-//       convo.end();
-// 	};
-
-// 	chat.conversation((convo) => {
-// 		askName(convo);
-// 	});
-// });
-
-bot.start();
+bot.start(process.env.PORT || 3000);
