0588f6d24d3100e7bd6d2ae61a8cff64d8221a9a
Apurba Mahanta <apurba5964@gmail.com>
2019-06-16 02:42:27 -0400

    Randomized Giphy

--- server/src/datasources/giphy.js
+++ server/src/datasources/giphy.js
@@ -20,11 +20,14 @@ gifReducer(response) {
 
 async getGifById({ search }) {
   //console.log(search)
-  const response = await this.get('search', { api_key: "Ky0tspF0p3JZsGCSt5D25iD11Q4rX0oB",limit : 1,
+  const response = await this.get('search', { api_key: "Ky0tspF0p3JZsGCSt5D25iD11Q4rX0oB",limit : 5,rating:"G",lang:"en",
   q:search });
   //console.log(response.data[0].images.original.url)
+  var min=0; 
+  var max=5;  
+  var random =Math.floor(Math.random() * (+max - +min)) + +min; 
 
-  return this.gifReducer(response.data[0].images.original.url);
+  return this.gifReducer(response.data[random].images.original.url);
 }
 
 
